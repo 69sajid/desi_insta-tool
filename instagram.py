528c033dc905d1783bc0847919ed37c12195ff75
69sajid <73467319+69sajid@users.noreply.github.com>
2021-06-13 18:21:12 +0530

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -1,3 +1,4 @@
+
 import telebot
 # import api
 import os
@@ -8,25 +9,29 @@ test = instaloader.Instaloader()
 API_KEY = "1797049159:AAEqWGHLn6qE8oRW_kQaSSxHhWXJkag4PgQ"
 bot = telebot.TeleBot(API_KEY, parse_mode=None)
 
-
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Hellllo baba...\nInsta account enter kijyye!!!")
+    bot.reply_to(message, "Hello ...\nEnter the user account")
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-
-    acc = message.text
-    p = test.download_profile(acc, profile_pic_only=True)
-    path = (os.path.join(os.getcwd(), acc))
-    for i in os.listdir(path):
-        if "jpg" in i or "jpeg" in i:
-            path = os.path.join(path, i)
-            break
-    im = Image.open(path)
-    # print(type(p))
-    bot.send_photo(message.chat.id, im, "Profile picture")
-
+    
+        acc = message.text
+        p=test.download_profile(acc, profile_pic_only=True)
+        path = (os.path.join(os.getcwd(), acc))
+        for i in os.listdir(path):
+            if "jpg" in i or "jpeg" in i:
+                path = os.path.join(path, i)
+                break
+        im = Image.open(path)
+        # print(type(p))
+        bot.send_photo(message.chat.id, im, "Profile picture")
 
 bot.polling()
+
+
+
+
+
+
